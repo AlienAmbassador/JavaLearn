f62b90719f90e8b0100cc30de15bd61e2ec40d2c
Lada Biriukova <biruyk.offf@yandex.ru>
2021-10-24 14:25:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+        boolean isGreenLight = false;
+
+        int speedOfPlayer1 = 0;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 0;
+        int cnt = 0;
+
+        if (isGreenLight) {
+            if (speedOfPlayer1 == 0) cnt++;
+            if (speedOfPlayer2 == 0) cnt++;
+            if (speedOfPlayer3 == 0) cnt++;
+        } else {
+            if (speedOfPlayer1 != 0) cnt++;
+            if (speedOfPlayer2 != 0) cnt++;
+            if (speedOfPlayer3 != 0) cnt++;
+        }
+
+        System.out.println(cnt);
+
+    }
+}
